bf499d071da91bde56ddb935fc925fddae87afde
Bianca ZY Cao <e0533381@u.nus.edu>
2024-10-20 17:05:03 +0800

    Fix: NULL value in audio features causing error

--- model_predicate.py
+++ model_predicate.py
@@ -98,12 +98,16 @@ def pickle_model_predict(model_cls, test_instance):
     cls_sign_map = {'neutral': 0, 'positive': 1, 'negative': -1}
     try:
         instance_input = np.array(test_instance).reshape(1, -1)
+        # Check if there are any NaN values in the instance input
+        if np.isnan(instance_input).any():
+            print("[ERROR] Skipping prediction due to NaN values in test instance.")
+            return 0, 0
         pred_cls = model_cls.predict(instance_input)[0]
         predictions_proba = model_cls.predict_proba(instance_input)
         max_prob = np.round(predictions_proba.max(axis=1), 4)[0]
         print("[pickle CLS MODEL]: ", pred_cls)
         pred_score = max_prob * cls_sign_map[pred_cls]
-        return pred_cls, pred_score
+        return max_prob, pred_score
     except Exception as e:
         raise Exception(f"Error during model prediction: {e}")
 
@@ -292,14 +296,14 @@ def audio_model_inference(x_input: np.ndarray):
         # this semester score - replace [-1,0,1] with scaled max_prob * [-1,0,1]
         sentiment_class_CNN, sentiment_score_CNN = CNN_Model_Predication_New(test_instance)
         print("CNN done; ", end='')
-        sentiment_class_RF, sentiment_score_RF = pickle_model_predict(RF_CLS_MODEL,test_instance)
+        sentiment_max_prob_RF, sentiment_score_RF = pickle_model_predict(RF_CLS_MODEL,test_instance)
         print("RF done; ", end='')
-        sentiment_class_LGBM, sentiment_score_LGBM = pickle_model_predict(LGBM_CLS_MODEL, test_instance)
-        print("[SCORE] CNN {:.2f}   RF {:.2f}   LGBM {:.2f}".format(
-            sentiment_score_CNN, sentiment_score_RF, sentiment_score_LGBM ))
+        sentiment_max_prob_LGBM, sentiment_score_LGBM = pickle_model_predict(LGBM_CLS_MODEL, test_instance)
         print("[TIME] - Audio models takes {:.2f} seconds".format(time.time() - start))
         combine_score = (sentiment_score_CNN + sentiment_score_RF + sentiment_score_LGBM)/3
-        print("[SCORE] final ".format(combine_score))
+        # max_abs_score = max(abs([sentiment_score_CNN, sentiment_score_RF, sentiment_score_LGBM])) # TODO
+        print("[SCORE] CNN {:.2f}   RF {:.2f}   LGBM {:.2f}   final {:.2f}".format(
+            sentiment_score_CNN, sentiment_score_RF, sentiment_score_LGBM, combine_score))
         sentiment_category = determine_sentiment_category(combine_score)
         # print("[TIME] - takes {:.2f} seconds".format(time.time() - start))
         if isinstance(combine_score, (int, float)):  # Check if it's an int or float
